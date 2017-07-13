c5f493983ee30d0399558e9ce28396bd5fd269a4
António Ferreira <antonioavf@gmail.com>
2017-07-13 14:39:17 +0100

    multi lang config

--- dato.config.js
+++ dato.config.js
@@ -35,7 +35,7 @@ module.exports = (dato, root, i18n) => {
   ['config.dev.toml', 'config.prod.toml'].forEach(file => {
     root.addToDataFile(file, 'toml', {
       title: dato.site.globalSeo.siteName,
-      languageCode: i18n.locale
+      //languageCode: i18n.locale
     });
   });
 
@@ -56,40 +56,62 @@ module.exports = (dato, root, i18n) => {
     seoMetaTags: toHtml(dato.home.seoMetaTags)
   });
 
-  // Create a markdown file with content coming from the `about_page` item
-  // type stored in DatoCMS
-  root.createPost(`content/about.md`, 'yaml', {
-    frontmatter: {
-      title: dato.aboutPage.title,
-      subtitle: dato.aboutPage.subtitle,
-      photo: dato.aboutPage.photo.url({ w: 800, fm: 'jpg', auto: 'compress' }),
-      seoMetaTags: toHtml(dato.aboutPage.seoMetaTags),
-      menu: { main: { weight: 100 } }
-    },
-    content: dato.aboutPage.bio
+
+  // iterate over all the available locales...
+  i18n.availableLocales.forEach((locale) => {
+
+    // switch to the nth locale
+    i18n.withLocale(locale, () => {
+
+      // Create a markdown file with content coming from the `about_page` item
+      // type stored in DatoCMS
+      root.createPost(`content/about.${locale}.md`, 'yaml', {
+        frontmatter: {
+          title: dato.aboutPage.title,
+          subtitle: dato.aboutPage.subtitle,
+          photo: dato.aboutPage.photo.url({ w: 800, fm: 'jpg', auto: 'compress' }),
+          seoMetaTags: toHtml(dato.aboutPage.seoMetaTags),
+          menu: { main: { weight: 100 } }
+        },
+        content: dato.aboutPage.bio
+      });
+
+    });
   });
+
 
   // Create a `work` directory (or empty it if already exists)...
   root.directory('content/work', dir => {
-    // ...and for each of the works stored online...
-    dato.works.forEach((work, index) => {
-      // ...create a markdown file with all the metadata in the frontmatter
-      dir.createPost(`${work.slug}.md`, 'yaml', {
-        frontmatter: {
-          title: work.title,
-          coverImage: work.coverImage.url({ w: 450, fm: 'jpg', auto: 'compress' }),
-          image: work.coverImage.url({ fm: 'jpg', auto: 'compress' }),
-          detailImage: work.coverImage.url({ w: 600, fm: 'jpg', auto: 'compress' }),
-          excerpt: work.excerpt,
-          seoMetaTags: toHtml(work.seoMetaTags),
-          extraImages: work.gallery.map(item =>
-            item.url({ h: 300, fm: 'jpg', auto: 'compress' })
-          ),
-          weight: index,
-        },
-        content: work.description
+
+    // iterate over all the available locales...
+    i18n.availableLocales.forEach((locale) => {
+
+      // switch to the nth locale
+      i18n.withLocale(locale, () => {
+
+        // ...and for each of the works stored online...
+        dato.works.forEach((work, index) => {
+          // ...create a markdown file with all the metadata in the frontmatter
+          dir.createPost(`${work.slug}.${locale}.md`, 'yaml', {
+            frontmatter: {
+              title: work.title,
+              coverImage: work.coverImage.url({ w: 450, fm: 'jpg', auto: 'compress' }),
+              image: work.coverImage.url({ fm: 'jpg', auto: 'compress' }),
+              detailImage: work.coverImage.url({ w: 600, fm: 'jpg', auto: 'compress' }),
+              excerpt: work.excerpt,
+              seoMetaTags: toHtml(work.seoMetaTags),
+              extraImages: work.gallery.map(item =>
+                item.url({ h: 300, fm: 'jpg', auto: 'compress' })
+              ),
+              weight: index,
+            },
+            content: work.description
+          });
+        });
+
       });
     });
+
   });
 };
 
